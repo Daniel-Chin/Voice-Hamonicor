a05728a35bab186dc94105119383de0b29268fa4
Daniel Chin <daniel_chin@yahoo.com>
2021-01-16 13:25:36 +0800

    better tester

--- testSynth.py
+++ testSynth.py
@@ -4,39 +4,30 @@ from harmonicSynth import Synth, Harmonic, TWO_PI
 from threading import Thread
 from time import sleep
 
-FRAME_LEN = 1024
+FRAME_LEN = 22100
 # FRAME_LEN = 2048*4
 SR = 22100
 FRAME_TIME = FRAME_LEN / SR
 
 DTYPE = (np.float32, pyaudio.paFloat32)
 
-class MyT(Thread):
-    def run(self):
-        while True:
-            self.s.eat(self.hs)
-            signal = self.s.mix()
-            # signal = DTYPE[0](np.sin(np.arange(FRAME_LEN) * TWO_PI * 220 / SR))
-            try:
-                self.stream.write(signal, FRAME_LEN)
-            except OSError:
-                print('thread exits')
-                return
-            sleep(FRAME_TIME)
+def onAudioOut(_, sample_count, *__):
+    s.eat(hs)
+    signal = s.mix()
+    print(sample_count, signal.size)
+    return (signal, pyaudio.paContinue)
 
 def main():
-    s = Synth(3, SR, FRAME_LEN, DTYPE[0], True, True)
+    global s, hs
+    s = Synth(1, SR, FRAME_LEN, DTYPE[0], True, True, .02)
+    hs = [Harmonic(220, 1) for _ in range(1)]
     pa = pyaudio.PyAudio()
     stream = pa.open(
         format = DTYPE[1], channels = 1, rate = SR, 
         output = True, frames_per_buffer = FRAME_LEN,
+        stream_callback = onAudioOut, 
     )
-    hs = [Harmonic(220, 1) for _ in range(3)]
-    thread = MyT()
-    thread.hs = hs
-    thread.s = s
-    thread.stream = stream
-    thread.start()
+    stream.start_stream()
     try:
         from console import console
         console({**locals(), **globals()})
